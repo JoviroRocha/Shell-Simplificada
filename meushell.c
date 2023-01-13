e92f473235ab66633e9dcc4c03f1588a73d25e05
Joviro Rocha <joviro.rocha@sap.com>
2023-01-13 16:22:01 -0300

    Update meushell.c

--- meushell.c
+++ meushell.c
@@ -10,19 +10,41 @@ int main(){
     printf(COLOR_GREEN "Configuring the shell...\n" COLOR_RESET);
     config();
     printf(COLOR_GREEN "Done configuring!\n" COLOR_RESET);
+    pthread_t id;
     while(1){
         // reseta as variáveis
-        reseta_variaveis();
+        reset_variables();
         // printa o PRONTO
         printf(COLOR_BLUE "%s $ " COLOR_RESET, PRONTO);
         // recebe o input
         scanf(" %[^\n]", data);
+        strcpy(data_save,data);
+        //save history
+        pthread_create(&id, NULL, add_history , NULL);
         // parseia o input
         parser();
         // executa o input
-        if( fork() == 0 ) execvp(path, variaveis);
-        wait(NULL);
-        
+        if (strcmp(variables[0], "cd") == 0){
+            cd();
+        }
+        else if (strcmp(variables[0],"clear") == 0){
+            clear();
+        } 
+        else if (strcmp(variables[0],"exit") == 0){
+            printf(COLOR_GREEN "Shell is exiting...\n" COLOR_RESET);
+            pthread_join(id, NULL);
+            return 0;
+        }
+        else {
+            int resp;
+            // Executar o comando
+           if( fork() == 0 ){
+                resp = execvp(path, variables);
+                if(resp == -1) printf(COLOR_RED "ERROR: Command not found\n" COLOR_RESET);
+                exit(0);
+           }
+            wait(NULL);
+        }
     };
     return 0;
 }
