5eddb71307798d7060872a894a2e634accc5ad2b
Joviro Rocha <joviro_neto@hotmail.com>
2023-01-17 17:50:14 -0800

    Revert "TEST"
    This reverts commit aee984d26a9b42bb0595633daa9d39d23a5598a7.

--- meushell.h
+++ meushell.h
@@ -10,8 +10,7 @@
 #define COLOR_RESET "\x1b[0m"
 
 char HOST[30];
-char PRONTO[256];
-char PRONTO_NOVO[256] = NULL;
+char *PRONTO;
 char DTA[256];
 char SHELL[30];
 char data[100];
@@ -71,7 +70,7 @@ void config()
     strcpy(file_path, DTA);
     strcat(file_path, "/.meushell.hst");
     // Sets PRONTO
-    strcpy(PRONTO, DTA);
+    PRONTO = DTA;
     // Get Shell name
     strcpy(SHELL, "Simplified Shell");
     // Write it to file
