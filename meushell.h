0410c14f89a4665980c9bc9e28bff0fa35644cf4
Joviro Rocha <joviro_neto@hotmail.com>
2023-01-20 13:00:39 -0800

    Update meushell.h

--- meushell.h
+++ meushell.h
@@ -376,7 +376,7 @@ int find_history()
         printf(COLOR_RED "ERROR: The file \".meushell.hst\" could not be found! \n" COLOR_RESET);
         exit(0);
     }
-    while(!feof(history_file)) fscanf(history_file, "%s", atual);
+    while(!feof(history_file)) fgets(atual, 99, history_file);
     printf("%s\n", atual);
     fclose(history_file);
     if(strcmp(atual,data) == 0){
